20590e3a7ed728b3af0b6fa3a372613356327e35
Shibe <alelo2006@live.com>
2024-01-17 19:42:18 -0500

    made xml support more/less than 8 cards
    it's messy

---
+++ src/commands/cardrarities.ts
@@ -0,0 +1,35 @@
+import Card from "../Structs/Interfaces/Card";
+import { parseToXML } from "../util/XML";
+import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+
+export default {
+    data: new SlashCommandBuilder()
+    .setName("cardrarities")
+    .setDescription("test the card rarities (yummy stats)"),
+
+    exec(interaction: ChatInputCommandInteraction) {
+        const HOLOPROB = {
+            "starlight": 1/1152,
+            "secret": 1/12,
+            "ultra": 1/6,
+            "super": 1/1
+        }
+        
+        let cards: Array<Card> = new Array<Card>;
+        cards = [
+            {ID: 1323, Name: "h"},
+            {ID: 4213, Name: "aaa"},
+            {ID: 5333, Name: "four"},
+            {ID: 8103, Name: "p"},
+        ];
+
+        let x = parseToXML(cards, 0);
+        console.log(x.deck.main.card);
+
+        interaction.reply({
+            
+            content: "yellow chat",
+            ephemeral: true
+        });
+    }
+}
